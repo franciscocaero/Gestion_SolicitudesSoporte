816193fc282db5cfc99d5c8faa9185ca412f3419
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-12-04 00:22:01 -0500

    Ajustes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'; 
 import usuarioRoutes from './routers/UsuarioRoutes.js';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors()); 
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.use('/api/usuarios', usuarioRoutes);
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
