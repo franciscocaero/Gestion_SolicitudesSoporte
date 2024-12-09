637476926989f692017df7f399b9f4f4a4d166a0
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-12-09 13:40:06 -0500

    Ajustes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import app from './server.js';
 import connection from './database.js';
 
+
 connection();
+
 
 export default (req, res) => {
   app(req, res);
 };
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+}
